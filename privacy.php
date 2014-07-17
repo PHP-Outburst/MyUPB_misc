fe3e0febbd18e79a4e56e3498eaf0f525e5bd909
Piotr Halama <halamix2@o2.pl>
2014-07-17 19:34:51 +0200

    Some files
    added privacy.php

---
+++ privacy.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+<head>
+<title>Privacy policy</title>
+<link rel='stylesheet' type='text/css'
+	href='skins/default/css/style.css' />
+</head>
+<body>
+<div id='upb_container'>
+<div class='main_cat_wrapper2'>
+<table class='main_table_2'>
+	<tr>
+		<td id='logo'><img src='skins/default/images/logo.png' alt='' title='' /></td>
+	</tr>
+</table>
+</div>
+<br />
+<br />
+<div class='main_cat_wrapper'>
+<div class='cat_area_1'>Privacy policy</div>
+<table class='main_table'>
+	<tr>
+		<td class='area_2'
+			style='text-align: center; font-weight: bold; padding: 12px; line-height: 20px;'>
+		Blahblahblah.<br />
+		<br />
+		<br /><br />
+			<a href="JavaScript:window.close()">Close window</a><br/>
+      </td>
+	</tr>
+	<tr>
+		<td class='footer_3'><img src='./skins/default/images/spacer.gif'
+			alt='' title='' /></td>
+	</tr>
+</table>
+<div class='footer'><img src='skins/default/images/spacer.gif' alt=''
+	title='' /></div>
+</div>
+</form>
+			<br />
+<div class='copy'><a href='http://forum.myupb.com/'> <?php
+require_once("./includes/upb.initialize.php");
+echo "Powered by myUPB v".UPB_VERSION."</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+	&copy; PHP Outburst 2002 - ".date("Y",time());
+?><br>
+<br>
+<a rel="license"
+	href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img
+	alt="Creative Commons License"
+	src="http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png"/>
+	</a></div>
+</div>
+</body>
+</html>
